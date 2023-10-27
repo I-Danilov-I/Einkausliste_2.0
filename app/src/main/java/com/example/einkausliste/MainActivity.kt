4cd8f5ae42ea595c4773a1c293d77dd9d1f5afa5
ARHITEKTOR <danilov@mail.de>
2023-10-27 02:59:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/einkausliste/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.einkausliste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MyLogAct!", "onCreate1")
+        Log.d("MyLogAct!", "onCreate2")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MyLogAct!", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MyLogAct!", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MyLogAct!", "onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MyLogAct!", "onDestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MyLogAct!", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MyLogAct!", "onRestart")
+    }
+}
