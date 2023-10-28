4a4d9b365d0f4357d64cdf52168a2fc06b87878d
ARHITEKTOR <danilov@mail.de>
2023-10-28 19:21:46 +0200

    Komentare gesetz

--- app/src/main/java/com/example/einkausliste/MainActivity.kt
+++ app/src/main/java/com/example/einkausliste/MainActivity.kt
@@ -9,58 +9,127 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
+import com.example.einkausliste.TextBlinker
 
 @UnstableApi
 class MainActivity : AppCompatActivity() {
-    lateinit var liste: ListView // Die ListView für die Einkaufsliste
-    lateinit var eingabeText: EditText // Das EditText-Feld für die Eingabe
-    lateinit var hinzufugenButton: Button // Die Schaltfläche zum Hinzufügen
-    lateinit var adapter: ArrayAdapter<String> // Ein Adapter für die ListView
-    val eintraege = mutableListOf("Wasser", "Kartoffel", "Sonnenblumenöl") // Eine Liste von Einträgen
-    lateinit var textBlinker: TextBlinker // Ein TextBlinker-Objekt für die Animation
+    lateinit var liste: ListView // Die ListView zur Anzeige der Einkaufsliste
+    lateinit var eingabeText: EditText // Das EditText-Feld zur Eingabe neuer Einträge
+    lateinit var hinzufugenButton: Button // Der Button zum Hinzufügen neuer Einträge
+    lateinit var adapter: ArrayAdapter<String> // Der Adapter für die ListView
+    val eintraege = mutableListOf("Wasser", "Kartoffel", "Sonnenblumenöl") // Die Liste der Einkäufe
+    lateinit var textBlinker: TextBlinker // Ein TextBlinker-Objekt zur Animation des EditText-Felds
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // Die Benutzeroberfläche der Activity festlegen
+        setContentView(R.layout.activity_main)
 
-        this.setupUI() // Die Benutzeroberfläche initialisieren
-        loadEinkaufsliste() // Die Einkaufsliste aus den gespeicherten Daten laden
-        Log.d("MyLogAct", "onCreate") // Log-Nachricht: Activity wurde erstellt
+        // Initialisierung der Benutzeroberfläche
+        this.setupUI()
+
+        // Laden der Einkaufsliste aus den SharedPreferences
+        loadEinkaufsliste()
+
+        // Log-Nachricht zur Anzeige in der Konsole
+        Log.d("MyLogAct", "onCreate")
     }
 
-    // Methode zum Speichern der Einkaufsliste
+    // Diese Funktion wird aufgerufen, wenn die Aktivität erstellt wird.
+
+    fun hinzufugen(view: View) {
+        // Extrahieren des eingegebenen Texts aus dem EditText-Feld
+        val neuerEintrag = eingabeText.text.toString()
+
+        // Überprüfung, ob der eingegebene Text nicht leer ist
+        if (neuerEintrag.isNotEmpty()) {
+            // Hinzufügen des neuen Eintrags zur Liste
+            adapter.add(neuerEintrag)
+            // Benachrichtigung des Adapters über die Änderung der Daten
+            adapter.notifyDataSetChanged()
+            // Löschen des Texts im EditText-Feld
+            eingabeText.text.clear()
+            // Speichern der aktualisierten Einkaufsliste in den SharedPreferences
+            saveEinkaufsliste()
+            // Log-Nachricht zur Anzeige in der Konsole
+            Log.d("MyLogAct", "Eintrag hinzugefügt: $neuerEintrag")
+        }
+    }
+
+    // Diese Funktion wird aufgerufen, wenn der "Hinzufügen"-Button geklickt wird.
+
     private fun saveEinkaufsliste() {
-        val eintraegeSet = HashSet(eintraege) // Die Einträge in ein Set umwandeln
-        val sharedPreferences = getSharedPreferences("Einkaufsliste", MODE_PRIVATE) // SharedPreferences initialisieren
-        val editor = sharedPreferences.edit() // Einen Editor für SharedPreferences erstellen
-        editor.putStringSet("einkaufsliste", eintraegeSet) // Die Einkaufsliste in SharedPreferences speichern
-        editor.apply() // Änderungen in SharedPreferences anwenden
-        Log.d("MyLogAct", "saveEinkaufsliste") // Log-Nachricht: Einkaufsliste wurde gespeichert
+        // Konvertieren der Liste der Einkäufe in ein Set
+        val eintraegeSet = HashSet(eintraege)
+
+        // Zugriff auf die SharedPreferences für die Einkaufsliste
+        val sharedPreferences = getSharedPreferences("Einkaufsliste", MODE_PRIVATE)
+
+        // Erstellen eines Editors für die SharedPreferences
+        val editor = sharedPreferences.edit()
+
+        // Speichern des Sets in den SharedPreferences
+        editor.putStringSet("einkaufsliste", eintraegeSet)
+        editor.apply() // Anwenden der Änderungen
+
+        // Log-Nachricht zur Anzeige in der Konsole
+        Log.d("MyLogAct", "Einkaufsliste gespeichert")
     }
 
-    // Methode zum Laden der Einkaufsliste
+    // Diese Funktion speichert die Einkaufsliste in den SharedPreferences.
+
     private fun loadEinkaufsliste() {
-        val sharedPreferences = getSharedPreferences("Einkaufsliste", MODE_PRIVATE) // SharedPreferences initialisieren
-        val savedEintraegeSet = sharedPreferences.getStringSet("einkaufsliste", null) // Einkaufsliste aus SharedPreferences laden
+        // Zugriff auf die SharedPreferences für die Einkaufsliste
+        val sharedPreferences = getSharedPreferences("Einkaufsliste", MODE_PRIVATE)
+
+        // Laden des Sets aus den SharedPreferences
+        val savedEintraegeSet = sharedPreferences.getStringSet("einkaufsliste", null)
 
         if (savedEintraegeSet != null) {
-            eintraege.clear() // Die aktuelle Liste leeren
-            eintraege.addAll(savedEintraegeSet) // Gespeicherte Einträge zur Liste hinzufügen
-            adapter.notifyDataSetChanged() // Den Adapter aktualisieren, um die Änderungen in der ListView anzuzeigen
+            // Überprüfung, ob gespeicherte Einträge vorhanden sind
+            eintraege.clear()
+            eintraege.addAll(savedEintraegeSet)
+            // Aktualisieren der ListView durch Benachrichtigung des Adapters
+            adapter.notifyDataSetChanged()
         }
-        Log.d("MyLogAct", "loadEinkaufsliste") // Log-Nachricht: Einkaufsliste wurde geladen
+
+        // Log-Nachricht zur Anzeige in der Konsole
+        Log.d("MyLogAct", "Einkaufsliste geladen")
     }
 
-    // ...
+    // Diese Funktion lädt die Einkaufsliste aus den SharedPreferences.
 
-    // Weitere Activity-Lebenszyklus-Methoden, hier nicht kommentiert
+    override fun onStart() {
+        super.onStart()
+        // Log-Nachricht zur Anzeige in der Konsole
+        Log.d("MyLogAct", "onStart")
+    }
 
-    // ...
+    override fun onResume() {
+        super.onResume()
+        // Log-Nachricht zur Anzeige in der Konsole
+        Log.d("MyLogAct", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Log-Nachricht zur Anzeige in der Konsole
+        Log.d("MyLogAct", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // Log-Nachricht zur Anzeige in der Konsole
+        Log.d("MyLogAct", "onStop")
+    }
 
     override fun onDestroy() {
-        saveEinkaufsliste() // Die Einkaufsliste speichern, bevor die Activity zerstört wird
-        textBlinker.stopBlinkAnimation() // Die TextBlinker-Animation stoppen
+        // Speichern der Einkaufsliste und Stoppen der Textanimation
+        saveEinkaufsliste()
+        textBlinker.stopBlinkAnimation()
         super.onDestroy()
-        Log.d("MyLogAct", "onDestroy") // Log-Nachricht: Activity wurde zerstört
+        // Log-Nachricht zur Anzeige in der Konsole
+        Log.d("MyLogAct", "onDestroy")
     }
 }
+
+// Diese Funktionen werden in verschiedenen Lebenszyklusphasen der Aktivität aufgerufen.
--- app/src/main/java/com/example/einkausliste/add.kt
+++ app/src/main/java/com/example/einkausliste/add.kt
@@ -8,16 +8,27 @@ import android.widget.ListView
 import android.util.Log
 
 fun hinzufugen(view: View) {
-    lateinit var eingabeText: EditText
-    lateinit var adapter: ArrayAdapter<String>
-    val eintraege = mutableListOf("Wasser", "Kartoffel", "Sonnenblumenöl")
+    // Deklaration der lokalen Variablen
+    lateinit var eingabeText: EditText // EditText-Feld für die Eingabe
+    lateinit var adapter: ArrayAdapter<String> // Adapter für die ListView
+    val eintraege = mutableListOf("Wasser", "Kartoffel", "Sonnenblumenöl") // Liste der Einträge
 
+    // Abrufen des eingegebenen Texts aus dem EditText-Feld
     val neuerEintrag = eingabeText.text.toString()
 
+    // Überprüfung, ob der eingegebene Text nicht leer ist
     if (neuerEintrag.isNotEmpty()) {
+        // Hinzufügen des neuen Eintrags zur Liste
         adapter.add(neuerEintrag)
+        // Benachrichtigung des Adapters über die Änderung der Daten
         adapter.notifyDataSetChanged()
+        // Löschen des Texts im EditText-Feld
         eingabeText.text.clear()
+        // Log-Nachricht zur Anzeige in der Konsole
         Log.d("MyLogAct", "com.example.einkausliste.com.example.einkausliste.hinzufugen")
     }
 }
+
+// Diese Funktion wird aufgerufen, wenn der "Hinzufügen"-Button in der Benutzeroberfläche
+// geklickt wird. Sie liest den eingegebenen Text aus dem EditText-Feld, fügt ihn zur Liste
+// hinzu, aktualisiert die ListView und zeigt eine Log-Nachricht an, wenn ein Eintrag hinzugefügt wird.
