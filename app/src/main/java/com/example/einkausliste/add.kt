9212f4f2d5a1c20a11182c100cce1336c3c57779
ARHITEKTOR <danilov@mail.de>
2023-10-29 13:40:23 +0100

    Hinzufügen ausgelagert

--- app/src/main/java/com/example/einkausliste/add.kt
+++ app/src/main/java/com/example/einkausliste/add.kt
@@ -1,9 +1,16 @@
 // add.kt-Datei
 package com.example.einkausliste
 
+// Importieren der Klasse "View" aus dem Android-Framework, die für die Benutzeroberfläche verwendet wird
 import android.view.View
+
+// Importieren der Klasse "ArrayAdapter" aus dem Android-Framework, um Daten an eine ListView zu binden
 import android.widget.ArrayAdapter
+
+// Importieren der Klasse "EditText" aus dem Android-Framework, die ein Texteingabefeld repräsentiert
 import android.widget.EditText
+
+// Importieren der Klasse "Log" aus dem Android-Framework, um Log-Nachrichten zu erstellen und anzuzeigen
 import android.util.Log
 
 fun hinzufugen(view: View, adapter: ArrayAdapter<String>, eingabeText: EditText, eintraege: MutableList<String>) {
